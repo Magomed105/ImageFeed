8148c41b26f610629e7e059baf9c7250085f2e26
Magomed <magomed.nullaev@yandex.ru>
2025-11-07 21:50:49 +0300

    start sprint_08

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

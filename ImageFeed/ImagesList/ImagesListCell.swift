0a04b00d47c1b0b580ebdb3efe597dd4a774a871
Magomed <magomed.nullaev@yandex.ru>
2025-11-15 20:35:49 +0300

    sprint_09 final

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,11 +1,7 @@
 import UIKit
 
-
-
 final class ImagesListCell: UITableViewCell {
-    
     static let reuseIdentifier = "ImagesListCell"
- 
     
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
